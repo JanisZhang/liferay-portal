1dc390ea27e20310f873de60458b6da63c6b7a4a
Janis Zhang <janis.zhang@liferay.com>
2022-04-21 15:10:43 +0800

    LPS-151923 Use Mockito only in HTMLImageAttachmentElementReplacerTest

--- modules/apps/upload/upload-web/src/test/java/com/liferay/upload/web/internal/HTMLImageAttachmentElementReplacerTest.java
+++ modules/apps/upload/upload-web/src/test/java/com/liferay/upload/web/internal/HTMLImageAttachmentElementReplacerTest.java
@@ -31,12 +31,11 @@ import org.junit.Test;
 
 import org.mockito.Mockito;
 
-import org.powermock.api.mockito.PowerMockito;
 
 /**
  * @author Alejandro Tardín
  */
-public class HTMLImageAttachmentElementReplacerTest extends PowerMockito {
+public class HTMLImageAttachmentElementReplacerTest  {
 
 	@ClassRule
 	@Rule
@@ -54,9 +53,9 @@ public class HTMLImageAttachmentElementReplacerTest extends PowerMockito {
 
 		};
 
-		_portletFileRepository = mock(PortletFileRepository.class);
+		_portletFileRepository = Mockito.mock(PortletFileRepository.class);
 
-		when(
+		Mockito.when(
 			_portletFileRepository.getPortletFileEntryURL(
 				Mockito.isNull(ThemeDisplay.class), Mockito.eq(_fileEntry),
 				Mockito.eq(StringPool.BLANK))
