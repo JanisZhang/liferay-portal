d7f45356f3b61ba417dec634dded6455e3ff4ecb
Janis Zhang <janis.zhang@liferay.com>
2022-08-02 14:59:52 +0800

    LPS-159499 SF, use portal service reference instead of 'com.liferay.portal.kernel.util.PortalUtil' in modules

--- modules/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountLocalServiceImpl.java
+++ modules/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountLocalServiceImpl.java
@@ -80,7 +80,7 @@ import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
@@ -302,7 +302,7 @@ public class CommerceDiscountLocalServiceImpl
 
 		Date date = new Date();
 
-		Date displayDate = PortalUtil.getDate(
+		Date displayDate = _portal.getDate(
 			displayDateMonth, displayDateDay, displayDateYear, displayDateHour,
 			displayDateMinute, user.getTimeZone(),
 			CommerceDiscountDisplayDateException.class);
@@ -310,7 +310,7 @@ public class CommerceDiscountLocalServiceImpl
 		Date expirationDate = null;
 
 		if (!neverExpire) {
-			expirationDate = PortalUtil.getDate(
+			expirationDate = _portal.getDate(
 				expirationDateMonth, expirationDateDay, expirationDateYear,
 				expirationDateHour, expirationDateMinute, user.getTimeZone(),
 				CommerceDiscountExpirationDateException.class);
@@ -1176,7 +1176,7 @@ public class CommerceDiscountLocalServiceImpl
 
 		Date date = new Date();
 
-		Date displayDate = PortalUtil.getDate(
+		Date displayDate = _portal.getDate(
 			displayDateMonth, displayDateDay, displayDateYear, displayDateHour,
 			displayDateMinute, user.getTimeZone(),
 			CommerceDiscountDisplayDateException.class);
@@ -1184,7 +1184,7 @@ public class CommerceDiscountLocalServiceImpl
 		Date expirationDate = null;
 
 		if (!neverExpire) {
-			expirationDate = PortalUtil.getDate(
+			expirationDate = _portal.getDate(
 				expirationDateMonth, expirationDateDay, expirationDateYear,
 				expirationDateHour, expirationDateMinute, user.getTimeZone(),
 				CommerceDiscountExpirationDateException.class);
@@ -1254,7 +1254,7 @@ public class CommerceDiscountLocalServiceImpl
 
 		Date date = new Date();
 
-		Date displayDate = PortalUtil.getDate(
+		Date displayDate = _portal.getDate(
 			displayDateMonth, displayDateDay, displayDateYear, displayDateHour,
 			displayDateMinute, user.getTimeZone(),
 			CommerceDiscountDisplayDateException.class);
@@ -1262,7 +1262,7 @@ public class CommerceDiscountLocalServiceImpl
 		Date expirationDate = null;
 
 		if (!neverExpire) {
-			expirationDate = PortalUtil.getDate(
+			expirationDate = _portal.getDate(
 				expirationDateMonth, expirationDateDay, expirationDateYear,
 				expirationDateHour, expirationDateMinute, user.getTimeZone(),
 				CommerceDiscountExpirationDateException.class);
@@ -1549,7 +1549,7 @@ public class CommerceDiscountLocalServiceImpl
 				new Date(), WorkflowConstants.STATUS_SCHEDULED);
 
 		for (CommerceDiscount commerceDiscount : commerceDiscounts) {
-			long userId = PortalUtil.getValidUserId(
+			long userId = _portal.getValidUserId(
 				commerceDiscount.getCompanyId(), commerceDiscount.getUserId());
 
 			ServiceContext serviceContext = new ServiceContext();
@@ -1577,7 +1577,7 @@ public class CommerceDiscountLocalServiceImpl
 
 		if ((commerceDiscounts != null) && !commerceDiscounts.isEmpty()) {
 			for (CommerceDiscount commerceDiscount : commerceDiscounts) {
-				long userId = PortalUtil.getValidUserId(
+				long userId = _portal.getValidUserId(
 					commerceDiscount.getCompanyId(),
 					commerceDiscount.getUserId());
 
@@ -1984,6 +1984,9 @@ public class CommerceDiscountLocalServiceImpl
 
 	@Reference
 	private ExpandoRowLocalService _expandoRowLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private WorkflowInstanceLinkLocalService _workflowInstanceLinkLocalService;
