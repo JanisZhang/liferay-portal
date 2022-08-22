9cb46a5819cfb4debc64471d0f69cb627acc5aac
Janis Zhang <janis.zhang@liferay.com>
2022-08-22 16:17:59 +0800

    LPS-161061 Implement abstract method 'getServletContext()' in CPDefinitionVirtualSettingDetailsTermsOfUseFormNavigatorEntry

--- modules/apps/commerce/commerce-product-type-virtual-web/src/main/java/com/liferay/commerce/product/type/virtual/web/internal/frontend/taglib/form/navigator/CPDefinitionVirtualSettingDetailsTermsOfUseFormNavigatorEntry.java
+++ modules/apps/commerce/commerce-product-type-virtual-web/src/main/java/com/liferay/commerce/product/type/virtual/web/internal/frontend/taglib/form/navigator/CPDefinitionVirtualSettingDetailsTermsOfUseFormNavigatorEntry.java
@@ -66,6 +66,11 @@ public class CPDefinitionVirtualSettingDetailsTermsOfUseFormNavigatorEntry
 	}
 
 	@Override
+	public ServletContext getServletContext() {
+		return null;
+	}
+
+	@Override
 	public void include(
 			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse)
