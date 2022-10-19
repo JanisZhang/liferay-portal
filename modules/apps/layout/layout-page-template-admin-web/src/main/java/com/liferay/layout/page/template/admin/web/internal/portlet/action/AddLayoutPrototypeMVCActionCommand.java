95b7155cfe758209101a31aa752d121b9b6236ea
Janis Zhang <janis.zhang@liferay.com>
2022-10-19 15:03:55 +0800

    LPS-166029 Remove unused reference field _portal, it has been referenced in LayoutPageTemplateEntryLocalServiceImpl

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddLayoutPrototypeMVCActionCommand.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddLayoutPrototypeMVCActionCommand.java
@@ -41,7 +41,6 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -207,9 +206,6 @@ public class AddLayoutPrototypeMVCActionCommand extends BaseMVCActionCommand {
 
 	@Reference
 	private LayoutPrototypeService _layoutPrototypeService;
-
-	@Reference
-	private Portal _portal;
 
 	private class AddLayoutPrototypeCallable
 		implements Callable<LayoutPrototype> {
