4fd92cd9075c23beea83cdddf44fa21792ac5266
Janis Zhang <janis.zhang@liferay.com>
2023-05-08 10:21:34 +0800

    LPS-166976 Convert method reference to serviceTrackerMap

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/configuration/LDAPConfigurationListener.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/configuration/LDAPConfigurationListener.java
@@ -56,12 +56,12 @@ public class LDAPConfigurationListener implements ConfigurationListener {
 				factoryPid, ".scoped", StringPool.BLANK);
 		}
 
-		if (!_serviceTrackerMap.containsKey(factoryPid)) {
-			return;
-		}
-
 		ConfigurationProvider<?> configurationProvider =
 			_serviceTrackerMap.getService(factoryPid);
+
+		if (configurationProvider == null) {
+			return;
+		}
 
 		try {
 			if (configurationEvent.getType() == ConfigurationEvent.CM_DELETED) {
