ebc53cdb5910ecd8da6f183a162b60321e79b267
Janis Zhang <janis.zhang@liferay.com>
2023-07-30 13:24:34 +0800

    LPS-192026 Reference WorkflowTaskManager in WorkflowTaskDisplayContext

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskPortlet.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskPortlet.java
@@ -19,7 +19,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowException;
 import com.liferay.portal.kernel.workflow.WorkflowTask;
-import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
 import com.liferay.portal.workflow.comparator.WorkflowComparatorFactory;
 import com.liferay.portal.workflow.security.permission.WorkflowTaskPermission;
 import com.liferay.portal.workflow.task.web.internal.configuration.WorkflowTaskWebConfiguration;
@@ -167,7 +167,7 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 			new WorkflowTaskDisplayContext(
 				_portal.getLiferayPortletRequest(renderRequest),
 				_portal.getLiferayPortletResponse(renderResponse),
-				_workflowComparatorFactory));
+				_workflowComparatorFactory, _workflowTaskManager));
 	}
 
 	private void _setWorkflowTaskRenderRequestAttribute(
@@ -181,8 +181,8 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 			ThemeDisplay themeDisplay =
 				(ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
 
-			WorkflowTask workflowTask = WorkflowTaskManagerUtil.getWorkflowTask(
-				themeDisplay.getCompanyId(), workflowTaskId);
+			WorkflowTask workflowTask = _workflowTaskManager.getWorkflowTask(
+				workflowTaskId);
 
 			_checkWorkflowTaskViewPermission(workflowTask, themeDisplay);
 
@@ -199,6 +199,9 @@ public class MyWorkflowTaskPortlet extends MVCPortlet {
 
 	@Reference
 	private WorkflowComparatorFactory _workflowComparatorFactory;
+
+	@Reference
+	private WorkflowTaskManager _workflowTaskManager;
 
 	@Reference
 	private WorkflowTaskPermission _workflowTaskPermission;
