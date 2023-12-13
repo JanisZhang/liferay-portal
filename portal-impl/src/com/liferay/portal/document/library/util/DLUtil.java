04c783f50a4c207b12cd1ca65307f32d1399d2ac
Janis Zhang <janis.zhang@liferay.com>
2023-12-13 14:11:42 +0800

    LPS-X Move DLUtil from portal-kernel to portal-impl, prepare for next.

--- portal-impl/src/com/liferay/portal/document/library/util/DLUtil.java
+++ portal-impl/src/com/liferay/portal/document/library/util/DLUtil.java
@@ -3,10 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.kernel.util;
+package com.liferay.portal.document.library.util;
 
 import com.liferay.document.library.kernel.model.DLFileEntryType;
 import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.util.DL;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
